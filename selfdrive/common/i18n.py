f3199bee005cea86fff72dd5c8d7340438f2ecf7
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-02-20 02:44:45 +0300

    get language info from Eon

--- selfdrive/common/i18n.py
+++ selfdrive/common/i18n.py
@@ -1,9 +1,12 @@
 import gettext
+from common import android
 
-
+supported_language = ['en-US', 'ar-EG']
 localedir = '../assets/locales'
-local = ['ar_EG']
-i18n = gettext.translation(localedir=localedir, fallback=True, languages=local)
+
+is_android = android.ANDROID
+local = android.getprop(ro.product.locale) #if is_android else 
+i18n = gettext.translation('messages', localedir=localedir, fallback=True, languages=local)
 
 i18n.install()
 
